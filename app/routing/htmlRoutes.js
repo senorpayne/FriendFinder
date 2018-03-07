92a04db5a2d3c38780e249ce68447d38f0639d75
Jason Payne <senorpayne@yahoo.com>
2018-03-07 14:56:45 -0600

    surveyquestions

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,6 +11,11 @@ app.get("/", function(req, res) {
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
+  app.get("/friends", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../data/friends.js"));
+  });
+
+
 
   app.use(function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
